342573e6abb72c39d628efe0140f92feb5a9e01a
Zazzaro <sassaro01cristian@gmail.com>
2023-10-31 11:45:10 +0100

    numbers get printed in the page now

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-for (let x = 1; x <= 100; x++) {
+document.addEventListener("DOMContentLoaded", function () {
+    for (let x = 1; x <= 100; x++) {
+        console.log(x);
+        const container = document.querySelector('.container');
 
-    if (x % 3 == 0 && x % 5 == 0) {
-        console.log("FizzBuzz")
-    } else if (x % 5 == 0){
-        console.log("Buzz")
-    } else if (x % 3 == 0) {
-        console.log("Fizz");
-    } else {
-        console.log(x)
+        if (x % 3 === 0 && x % 5 === 0) {
+            console.log("FizzBuzz");
+            let paragraph = document.createElement("p");
+            paragraph.innerHTML = "FizzBuzz";
+            container.appendChild(paragraph);
+        } else if (x % 5 === 0) {
+            console.log("Buzz");
+            let paragraph = document.createElement("p");
+            paragraph.innerHTML = "Buzz";
+            container.appendChild(paragraph);
+        } else if (x % 3 === 0) {
+            console.log("Fizz");
+            let paragraph = document.createElement("p");
+            paragraph.innerHTML = "Fizz";
+            container.appendChild(paragraph);
+        } else {
+            let paragraph = document.createElement("p");
+            paragraph.innerHTML = x;
+            container.appendChild(paragraph);
+        }
     }
-}
-
+});
